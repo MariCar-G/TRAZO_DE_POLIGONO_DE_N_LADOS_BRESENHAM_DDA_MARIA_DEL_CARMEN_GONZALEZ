e3e7e0cfc0ef20009820ad374a12edefef0737fb
MariCar-G <61396417+MariCar-G@users.noreply.github.com>
2021-06-24 11:33:54 -0500

    Add files via upload

---
+++ TrazoDePoligono_MariaDelCarmenGonzalezArce_3061.py
@@ -0,0 +1,63 @@
+from turtle import Turtle
+import subprocess
+import bresenham
+
+def main():
+    subprocess.call(["cmd.exe","/C","cls"])
+    t=Turtle()
+    t.hideturtle()
+    arregloX = []
+    arregloY = []
+    x1= 0
+    y1= 0
+    x2= 20
+    y2= 20
+    tipo = int(input("ELIGUE EL ALGORITMO QUE GRAFICARA \n1.DDA \n2.BRESENHAM\n=>"))
+
+    
+    dx= abs(x2-x1)
+    dy= abs(y2-y1)
+    p= (2 * dy) - dx
+
+    t.color("white")
+    t.setx(dx)
+    t.sety(dy)
+    t.color("black")
+
+    t.pensize("5")
+    n= int(input("REGISTRA EL NUMERO DE LADOS: "))
+    ang=180-(((n-2)/n)*180)
+    if tipo == 1:
+        if n <= 306:
+            for i in range(n):
+                t.left(ang)
+                x1=x1+1
+                if p < 0:
+                    p = p + (2 * dy)
+                else:
+                    p = p + (2 * dy) - (2 * dx)
+                    y1=y1+1
+                print(x1, ',', y1)
+                t.fd(p)
+        else:
+            print("LOS LADOS NO PUEDEN SER MAYORES A 360")
+    if tipo == 2:
+        if n <= 306:
+            for i in range(n):
+                t.left(ang)
+                x1=x1+1
+                if p < 0:
+                    p = p + (2 * dy)
+                else:
+                    p = p + (2 * dy) - (2 * dx)
+                    y1=y1+1
+                print(x1, ',', y1)
+                t.fd(p)
+        else:
+            print("LOS LADOS NO PUEDEN SER MAYORES A 360")
+        
+
+    
+
+if __name__== '__main__':
+    main()
